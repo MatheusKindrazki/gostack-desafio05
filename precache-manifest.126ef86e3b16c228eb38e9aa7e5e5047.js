6cadf496e33a07418110ad2972bbea3f86f35065
Matheus Kindrazki <matheus@mentores.com.br>
2019-09-05 00:25:13 -0300

    Updates

---
+++ precache-manifest.126ef86e3b16c228eb38e9aa7e5e5047.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d06c87db29cb7283cd1cc94f88eadba",
+    "url": "/gostack-desafio05/index.html"
+  },
+  {
+    "revision": "f3ee3467bc5b20c62e78",
+    "url": "/gostack-desafio05/static/js/2.66f87cb4.chunk.js"
+  },
+  {
+    "revision": "500f80925fe1bf53ba2b",
+    "url": "/gostack-desafio05/static/js/main.6f475776.chunk.js"
+  },
+  {
+    "revision": "f8bdab5e47febf1af9af",
+    "url": "/gostack-desafio05/static/js/runtime~main.8016772b.js"
+  }
+]);
